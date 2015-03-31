1f7b5735a348e1b4aa325c99bc2e7b55ef4f6cfb
Victor Gan <victorgan@gmail.com>
2015-03-30 22:14:41 -0700

    applying Voxel Grid Filter

--- src/my_pcl_tutorial/src/example.cpp
+++ src/my_pcl_tutorial/src/example.cpp
@@ -4,19 +4,32 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
 
 ros::Publisher pub;
 
 void 
-cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
+cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
-  // Create a container for the data.
+  // Container for original & filtered data
+  pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
+  pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
+  pcl::PCLPointCloud2 cloud_filtered;
+
+  // Convert to PCL data type
+  pcl_conversions::toPCL(*cloud_msg, *cloud);
+
+  // Perform the actual filtering
+  pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+  sor.setInputCloud (cloudPtr);
+  sor.setLeafSize (0.1, 0.1, 0.1);
+  sor.filter (cloud_filtered);
+
+  // Convert to ROS data type
   sensor_msgs::PointCloud2 output;
+  pcl_conversions::fromPCL(cloud_filtered, output);
 
-  // Do data processing here...
-  output = *input;
-
-  // Publish the data.
+  // Publish the data
   pub.publish (output);
 }
 
