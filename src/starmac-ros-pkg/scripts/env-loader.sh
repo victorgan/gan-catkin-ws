8bc5ad1cde930fa6d96e4cb752dfc85c53d7e85c
Victor Gan <victorgan@gmail.com>
2015-08-13 12:11:47 -0700

    removed unused packages

--- src/starmac-ros-pkg/scripts/env-loader.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. /opt/ros/fuerte/setup.sh
-# ROS path stuff
-source ~/ros/starmac-ros-pkg/scripts/set-env-vars.sh
-viper_pelican $(cat ~/ros/starmac-ros-pkg/scripts/ros_master_host_name)
-exec "$@"
